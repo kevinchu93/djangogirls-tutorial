4100e82070b1eadcb629d6a1c2e650aa71b7fa05
Kevin Chu <kevinxchu@gmail.com>
2017-12-17 14:19:11 -0800

    added small tweaks and delete post function

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,12 @@
 from django import forms
-
 from .models import Post
 
-class PostForm(forms. ModelForm):
+class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
         fields = ('title', 'text',)
+        widgets = {
+                'text': forms.TextInput(attrs={'Placeholder': 'teriyaki chicken'}),
+                'title': forms.TextInput(attrs={'Placeholder': 'titl'})
+                }
